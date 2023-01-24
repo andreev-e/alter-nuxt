a51b7e151a98bfc0bf5dff9267ddc52e866e8946
e.andreev <k9prP46sXJ>
2023-01-24 23:22:55 +0300

    State

--- store/tags.js
+++ store/tags.js
@@ -1,9 +1,12 @@
 export const state = () => ({
     tags: [],
+    tagsLoading: false,
     countries: [],
 });
 
 export const getters = {
+    // eslint-disable-next-line no-shadow
+    tagsLoading: (state) => state.tagsLoading,
     // eslint-disable-next-line no-shadow
     tagsExist: (state) => state.tags.length > 0,
     // eslint-disable-next-line no-shadow
@@ -13,8 +16,10 @@ export const getters = {
 export const mutations = {
     // eslint-disable-next-line no-shadow
     async getTags(state) {
+        state.tagsLoading = true;
         const res = await this.$axios.$get('/tag');
         state.tags = res.data;
+        state.tagsLoading = false;
     },
     // eslint-disable-next-line no-shadow
     async getCountries() {
