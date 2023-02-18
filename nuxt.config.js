fbcfcf4612db858e832684f86c50389d17d4b3e6
e.andreev <k9prP46sXJ>
2023-02-18 12:04:04 +0300

    No https

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,10 +97,10 @@ export default {
     },
 
     server: {
-        https: {
-            key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
-            cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt')),
-        },
+        // https: {
+        //     key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
+        //     cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt')),
+        // },
     },
 
     serverMiddleware: ['~/server-middleware/logger'],
