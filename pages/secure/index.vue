08899d9e31344ba0ce73143ff870c92cd1ac09e0
e.andreev <k9prP46sXJ>
2023-02-20 18:17:45 +0300

    Routes tab

--- pages/secure/index.vue
+++ pages/secure/index.vue
@@ -8,37 +8,80 @@
                     Авторский раздел
                 </h1>
                 <client-only>
-                    <template v-if="pois.length">
-                        <item-gallery
-                            :objects="pois"
-                            :loading="poiLoading"
-                            @reload="fetchPois"
-                        />
-                        <div class="row">
-                            <div class="col-12">
-                                <b-pagination
-                                    v-if="meta.last_page > 1"
-                                    v-model="page"
-                                    :total-rows="meta.total"
-                                    :per-page="meta.per_page"
-                                    aria-controls="my-table"
+                    <b-tabs>
+                        <b-tab
+                            title="Точки"
+                            class="my-3"
+                        >
+                            <template v-if="pois.length">
+                                <item-gallery
+                                    :objects="pois"
+                                    :loading="poiLoading"
+                                    @reload="fetchPois"
                                 />
-                            </div>
-                        </div>
-                    </template>
-                    <router-link
-                        v-else
-                        to="/secure/poi/create"
-                        class="d-inline-block mr-1 mt-1"
-                        title="Добавить достопримечательность"
-                    >
-                        <font-awesome-icon
-                            icon="fa-plus-circle"
-                            class="text-success"
-                            role="button"
-                        />
-                        Добавить достопримечательность
-                    </router-link>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <b-pagination
+                                            v-if="meta.last_page > 1"
+                                            v-model="page"
+                                            :total-rows="meta.total"
+                                            :per-page="meta.per_page"
+                                            aria-controls="my-table"
+                                        />
+                                    </div>
+                                </div>
+                            </template>
+                            <router-link
+                                v-else
+                                to="/secure/poi/create"
+                                class="d-inline-block mr-1 mt-1"
+                                title="Добавить достопримечательность"
+                            >
+                                <font-awesome-icon
+                                    icon="fa-plus-circle"
+                                    class="text-success"
+                                    role="button"
+                                />
+                                Добавить достопримечательность
+                            </router-link>
+                        </b-tab>
+                        <b-tab
+                            title="Маршруты"
+                            class="my-3"
+                        >
+                            <template v-if="routes.length">
+                                <item-gallery
+                                    :objects="routes"
+                                    :loading="routesLoading"
+                                    @reload="fetchRoutes"
+                                />
+                                <div class="row">
+                                    <div class="col-12">
+                                        <b-pagination
+                                            v-if="routesMeta.last_page > 1"
+                                            v-model="routesPage"
+                                            :total-rows="routesMeta.total"
+                                            :per-page="routesMeta.per_page"
+                                            aria-controls="my-table"
+                                        />
+                                    </div>
+                                </div>
+                            </template>
+                            <router-link
+                                v-else
+                                to="/secure/route/create"
+                                class="d-inline-block mr-1 mt-1"
+                                title="Добавить маршрут"
+                            >
+                                <font-awesome-icon
+                                    icon="fa-plus-circle"
+                                    class="text-success"
+                                    role="button"
+                                />
+                                Добавить маршрут
+                            </router-link>
+                        </b-tab>
+                    </b-tabs>
                 </client-only>
             </div>
         </div>
@@ -58,6 +101,7 @@
         data() {
             return {
                 page: 1,
+                routesPage: 1,
                 breadCrumbs: [
                     {
                         name: 'Авторский раздел',
@@ -74,21 +118,32 @@
                 poiLoading: 'poisPaginated/loading',
                 pois: 'poisPaginated/items',
                 meta: 'poisPaginated/meta',
+                routesLoading: 'routesPaginated/loading',
+                routes: 'routesPaginated/items',
+                routesMeta: 'routesPaginated/meta',
             }),
         },
         watch: {
             page() {
                 this.fetchPois();
             },
+            routesPage() {
+                this.fetchRoutes();
+            },
         },
         mounted() {
             this.fetchPois();
+            this.fetchRoutes();
         },
         methods: {
             ...mapActions({
                 getPoi: 'poisPaginated/get',
                 setParams: 'poisPaginated/setParams',
                 clear: 'poisPaginated/clear',
+
+                getRoutes: 'poisPaginated/get',
+                setParamsRoutes: 'poisPaginated/setParams',
+                clearRoutes: 'poisPaginated/clear',
             }),
             fetchPois() {
                 this.clear();
@@ -99,6 +154,16 @@
                     withHidden: 1,
                 });
                 this.getPoi();
+            },
+            fetchRoutes() {
+                this.clearRoutes();
+                this.setParamsRoutes({
+                    user: this.$auth.user.username,
+                    page: this.page,
+                    latest: 1,
+                    withHidden: 1,
+                });
+                this.getRoutes();
             },
         },
     };
