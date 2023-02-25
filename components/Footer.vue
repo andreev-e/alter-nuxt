3bb6d610bd4f019ef4c1e3a5829458081ad9e605
e.andreev <k9prP46sXJ>
2023-02-25 13:48:33 +0300

    Metrika

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,25 +1,33 @@
 <template>
     <div class="row footer">
         <div class="first_menu col-sm-4">
-            <client-only>
-                <!-- Yandex.Metrika counter -->
-                <script type="text/javascript">
-                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                    m[i].l=1*new Date();
-                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            <!-- Yandex.Metrika counter -->
+            <script type="text/javascript">
+                (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+                }
+                m[i].l = 1 * new Date()
+                for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                return
+                }
+                }
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+                })
+                (window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
 
-                    ym(10896850, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                    });
-                </script>
-                <noscript><div><img src="https://mc.yandex.ru/watch/10896850" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-                <!-- /Yandex.Metrika counter -->
-            </client-only>
+                ym(10896850, 'init', {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true,
+                })
+            </script>
+            <noscript>
+                <div><img src="https://mc.yandex.ru/watch/10896850" style="position:absolute; left:-9999px;" alt="" /></div>
+            </noscript>
+            <!-- /Yandex.Metrika counter -->
         </div>
         <div class="copyright col-sm-4">
             2009-{{ year }} © Альтернативный путеводитель
