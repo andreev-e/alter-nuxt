ec292b016d9385c7d8c281a8d4e1f2d3a73d7a85
e.andreev <k9prP46sXJ>
2026-06-22 16:43:04 +0300

    redeploy

--- components/SuperGallery.vue
+++ components/SuperGallery.vue
@@ -4,9 +4,9 @@
             v-for="img in images"
             :key="img.id"
             class="frame"
-            :style="`width:${img.width*heightOfRow/img.height}px;flex-grow:${img.width*heightOfRow/img.height}`"
+            :style="frameStyle(img)"
         >
-            <i :style="`padding-bottom:${img.height/img.width*100}%`" />
+            <i :style="paddingStyle(img)" />
             <a
                 class="cursor-pointer"
                 @click.prevent="view(img)"
@@ -52,6 +52,11 @@
                 type: Number,
                 default: 250,
             },
+            // Новый проп – соотношение сторон по умолчанию (ширина/высота)
+            defaultAspectRatio: {
+                type: Number,
+                default: 1, // 1:1
+            },
         },
         computed: {
             heightOfRow() {
@@ -59,13 +64,39 @@
             },
         },
         methods: {
+            // Возвращает соотношение сторон для конкретного изображения
+            getAspectRatio(img) {
+                if (img.width && img.height && img.width > 0 && img.height > 0) {
+                    return img.width / img.height;
+                }
+                return this.defaultAspectRatio;
+            },
+            // Стили для контейнера .frame
+            frameStyle(img) {
+                const ratio = this.getAspectRatio(img);
+                const width = this.heightOfRow * ratio;
+                return {
+                    width: `${width}px`,
+                    flexGrow: `${width}`,
+                };
+            },
+            // Стили для псевдо-элемента i (padding-bottom для сохранения пропорций)
+            paddingStyle(img) {
+                const ratio = this.getAspectRatio(img);
+                const paddingBottom = (1 / ratio) * 100;
+                return {
+                    paddingBottom: `${paddingBottom}%`,
+                };
+            },
             view(img) {
                 if (img.href) {
                     this.$router.push(img.href);
                 } else {
                     this.$msgbox({
                         dangerouslyUseHTMLString: true,
-                        message: `<div style="background-color: ${this.dominateColor ? this.dominateColor : '#606084'}"><img class="img-fluid" src="${img.original}"></div>`,
+                        message: `<div style="background-color: ${
+                            this.dominateColor ? this.dominateColor : '#606084'
+                        }"><img class="img-fluid" src="${img.original}"></div>`,
                         showConfirmButton: false,
                         center: true,
                         lockScroll: false,
@@ -80,49 +111,53 @@
 </script>
 
 <style scoped>
-  section {
-    display: flex;
-    flex-wrap: wrap;
-    margin: -3px;
-  }
+    section {
+        display: flex;
+        flex-wrap: wrap;
+        margin: -3px;
+    }
 
-  section::after {
-    content: '';
-    flex-grow: 999999999;
-  }
+    section::after {
+        content: "";
+        flex-grow: 999999999;
+    }
 
-  div {
-    margin: 3px;
-    background-color: #606084;
-    position: relative;
-  }
+    .frame {
+        margin: 3px;
+        background-color: #606084;
+        position: relative;
+        overflow: hidden; /* чтобы обрезать выступающие части при object-fit: cover */
+    }
 
-  i {
-    display: block;
-  }
+    i {
+        display: block;
+    }
 
-  img {
-    position: absolute;
-    top: 0;
-    width: 100%;
-    vertical-align: bottom;
-  }
+    img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover; /* заполняет контейнер без искажений */
+        vertical-align: bottom;
+    }
 
-  .frame .position-absolute {
-    display: none;
-  }
+    .frame .position-absolute {
+        display: none;
+    }
 
-  .frame:hover .position-absolute {
-    display: block;
-  }
+    .frame:hover .position-absolute {
+        display: block;
+    }
 
-  .position-absolute {
-    background-color: #FFFFFF;
-    color: #244255;
-    top: -3px;
-    right: -3px;
-    z-index: 2;
-    opacity: 0.8;
-    padding: 3px 10px;
-  }
+    .position-absolute {
+        background-color: #ffffff;
+        color: #244255;
+        top: -3px;
+        right: -3px;
+        z-index: 2;
+        opacity: 0.8;
+        padding: 3px 10px;
+    }
 </style>
